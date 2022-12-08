d303388b8392c4632aec447c70638357a7d7217b
Samandar <olmasovsamandar120@gmail.com>
2022-12-08 22:24:31 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿System.Console.Write("Enter your name: ");
+﻿ System.Console.Write("Enter your name: ");
  string name = Console.ReadLine();
  string greeting =$"Hello, {name}";
  System.Console.WriteLine(greeting);
 
-System.Console.WriteLine("Enter your age: ");
+ System.Console.WriteLine("Enter your age: ");
  string ageAsString =Console.ReadLine();
  int age =Convert.ToInt32(ageAsString);
  System.Console.WriteLine("Converting...");
@@ -12,6 +12,15 @@ System.Console.WriteLine("Enter your age: ");
  int keschaAge  =3;
  int ageDiffrence=age- keschaAge;
  System.Console.WriteLine($"The diffrence between your and Kescha,s {ageDiffrence} ");
+ // >>= <<=  ==  !=
+ System.Console.WriteLine($"You are older than or equal to KESCHA: {age >= keschaAge}");
+ System.Console.WriteLine($"You are older to than KESCHA: {age > keschaAge}");
+ System.Console.WriteLine($"You are younger than  or equal to KESCHA: {age < keschaAge}");
+ System.Console.WriteLine($"You are younger than KESCHA: {age <= keschaAge}");
+ System.Console.WriteLine($"You age is equal to KESCHA: {age == keschaAge}");
+ System.Console.WriteLine($"You age is not equal to KESCHA: {age != keschaAge}");
+
+
 
 
 
