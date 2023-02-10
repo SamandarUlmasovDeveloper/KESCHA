da36d858e6778cfd6dd49a2b752d7bad4d52ff3e
Samandar <olmasovsamandar120@gmail.com>
2023-02-10 18:26:12 +0500

    INFRA: init project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿ System.Console.Write("Enter your name: ");
+﻿string kod = "";
+do{
+   System.Console.WriteLine("Enter password!");
+   kod = Console.ReadLine();
+   }
+while(kod != "123");
+
+ System.Console.Write("Enter your name: ");
  string name = Console.ReadLine();
  string greeting =$"Hello, {name}";
  System.Console.WriteLine(greeting);
@@ -9,16 +16,63 @@
  System.Console.WriteLine("Converting...");
  System.Console.WriteLine($"successfully!{age}");
 
- int keschaAge  =3;
- int ageDiffrence=age- keschaAge;
+ 
+ 
+
+ int keschasAge  =3;
+ int ageDiffrence=age- keschasAge;
  System.Console.WriteLine($"The diffrence between your and Kescha,s {ageDiffrence} ");
- // >>= <<=  ==  !=
- System.Console.WriteLine($"You are older than or equal to KESCHA: {age >= keschaAge}");
- System.Console.WriteLine($"You are older to than KESCHA: {age > keschaAge}");
- System.Console.WriteLine($"You are younger than  or equal to KESCHA: {age < keschaAge}");
- System.Console.WriteLine($"You are younger than KESCHA: {age <= keschaAge}");
- System.Console.WriteLine($"You age is equal to KESCHA: {age == keschaAge}");
- System.Console.WriteLine($"You age is not equal to KESCHA: {age != keschaAge}");
+
+
+string message = 
+     keschasAge >= 0
+         ?"You are older"
+         :"You are younger";     
+
+ if(age > keschasAge)
+ {
+     System.Console.WriteLine("You are older");
+}
+else if (age == keschasAge)
+{
+  
+  System.Console.WriteLine("You are equal");
+}
+else 
+{
+  System.Console.WriteLine("You are younger");
+}
+
+System.Console.WriteLine("Lett me tell you about my friends.");
+string [] friendsName = { "Vasya", "Kasee", "Valya"};
+System.Console.WriteLine(friendsName[2]);
+int  [] friendsAge = {1, 2, 3,};
+
+string[] friendName = new string[4];
+friendsName[0] ="kasee";
+friendsName[1] ="vasya";
+friendsName[2] =name;
+int[] friendAge ={1, 2, age};
+
+System.Console.WriteLine($" {friendsName[0]} is {friendsAge[0]} year old!");
+System.Console.WriteLine($" {friendsName[1]} is {friendsAge[1]} year old!");
+System.Console.WriteLine($" {friendsName[2]} is {friendsAge[2]} year old!");
+
+ for(int iteration = 0; iteration < friendsName.Length; iteration++)
+ {
+    System.Console.WriteLine($"{friendsName [iteration] } is {friendsAge[iteration]} year old");
+ }
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
